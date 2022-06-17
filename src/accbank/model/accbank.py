46fa5f8e443ee6898748a055995642c72bad7e90
carvalhoabel <abelbdecarvalho@gmail.com>
2022-06-17 19:58:07 -0300

    Field type added to Account Bank

--- src/accbank/model/accbank.py
+++ src/accbank/model/accbank.py
@@ -12,6 +12,7 @@ class AccBank(BankGeneral):
         """
         super().__init__()
         self._agency = self._number = self._id_acc = 0
+        self._type = ''
 
     @property
     def agency(self) -> int:
@@ -28,6 +29,26 @@ class AccBank(BankGeneral):
     @number.setter
     def number(self, number: int):
         self._number = number
+
+    @property
+    def type(self) -> str:
+        return self._type
+
+    @type.setter
+    def type(self, type: int):
+        """
+        This method is special because it looks for the right
+        account type available.
+        - Payment -> 1
+        - Checking -> 2
+        - Saving -> 3
+        :param type: int. The key to choose.
+        :return: None.
+        """
+        if type < 1 or type > 3:
+            return None
+        bank = ('Payment', 'Checking', 'Saving')
+        self._type = bank[type]
 
     @property
     def id_acc(self) -> int:
